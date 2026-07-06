9cffd326c57c459b20f15a8decb6b0ac2de41804
sangg <sangg@DESKTOP-8J8G7VO>
2026-07-06 15:53:19 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Infass_Vequiso.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
