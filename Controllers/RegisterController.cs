2698fdf11be5960e320d6937d2df6eb7bbb1c5b2
dkGaban <ccs.dannel@gmail.com>
2026-07-14 11:00:47 +0800

    Login and Register

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Infass_Vequiso.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
